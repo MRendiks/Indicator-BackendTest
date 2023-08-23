0d5be12262f328d13c30c0d253ef627d6cb0b0e3
Rendaks <105725708+MRendiks@users.noreply.github.com>
2023-08-23 19:38:18 +0700

    add answers to conceptual questions

--- Is_palindrome/is_palindrome.java
+++ Is_palindrome/is_palindrome.java
@@ -4,10 +4,12 @@ class CheckIsPalindrome
 {
     public static void main(String[] args) 
     {
+        // Input data
         Scanner input = new Scanner(System.in);
         System.out.print("Input a word : ");
         String inputString = input.nextLine();
 
+        // check is polindrom using function
         if (IsPalindrome(inputString)) {
             System.out.println("The word is a palindrome.");
         } else {
@@ -19,9 +21,12 @@ class CheckIsPalindrome
 
     public static boolean IsPalindrome(String data)
     {
+        // define user input and clean data from characters other than letters and lowercase data
         data = data.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        // defines a variable for checking palindrome data
         String reverse = "";
         int length = data.length();   
+        // checks whether the reverse variable is the same as the user input data
         for ( int i = length - 1; i >= 0; i-- )  
             reverse = reverse + data.charAt(i);  
         if (data.equals(reverse))  
